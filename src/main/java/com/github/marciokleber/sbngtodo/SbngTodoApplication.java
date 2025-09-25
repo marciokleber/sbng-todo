87d74bdca5cee1eee30fe231b96589e69080b75e
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2025-09-25 20:52:43 -0300

    First commit: Projeto To Do usando springboot e angular

---
+++ src/main/java/com/github/marciokleber/sbngtodo/SbngTodoApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.sbngtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbngTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbngTodoApplication.class, args);
+    }
+
+}
